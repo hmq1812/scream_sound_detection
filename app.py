3db49982fe61f93f0ebb63253f168036553d7532
Minh Quan Huynh <hmquan1812@gmail.com>
2022-11-08 14:19:20 +1100

    final

--- app.py
+++ app.py
@@ -1,19 +1,56 @@
 from flask import Flask
 from flask import request
 import json
+import librosa
+from io import BytesIO
+from urllib.request import urlopen
+
+
+ALLOWED_PROFILE_EXTENSIONS = {'mp3'}
+
+def allowed_file(filename):
+    file_type = filename.split(".")[1]
+    if file_type in ALLOWED_PROFILE_EXTENSIONS:
+        return True
+    return False
 
 app = Flask(__name__)
 
-@app.route('/main', methods=['POST', 'OPTIONS'])
+@app.route('/main', methods=['POST'])
 def main():
-
+    # Check if file is sent from the user
+    if 'file' not in request.files:
+        return {
+            "message": "no file found",
+            "error_code": 400
+        }, 400
+    
+    # Get file
     file = request.files['file']
-    print(file)
+    
+    if file is None:
+        return {
+            "message": "no file found",
+            "error_code": 400
+        }, 400
 
-    return { "ok": True }
+    if not allowed_file(file.filename):
+        return {
+            "message": "file type is not allowed",
+            "error_code": 400
+        }, 400
+
+    file.save(file.filename)
+    file_name = f"{file.filename}"
+    print(file_name)
+    clip, sample_rate = librosa.load(f"{file.filename}", sr=44100) 
+    print(clip, sample_rate)
+    # rs = model.predict(clip, sample_rate)
+    return {"message": "success", "error_code": "200"}, 200
+
 
 if __name__ == '__main__':
-    app.run(host = 'localhost', port = 8000)
+    app.run(host = 'localhost', port = 8000, debug=True)
 
 
 # from fileinput import filename
